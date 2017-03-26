d89775b1d9b3b60861ff1d22cb7778c81db5cadf
positiwee <positiwee@gmail.com>
2017-03-26 17:33:17 +0900

    initial commit/ jetty setting / gif encoder setting, test complete / gitignore setting

---
+++ src/main/java/com/inha/iter/HomeController.java
@@ -0,0 +1,70 @@
+package com.inha.iter;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.imageio.ImageIO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 * @throws IOException 
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) throws IOException {
+		logger.info("This is Team Iter Project.", locale);
+		
+		 AnimatedGifEncoder e = new AnimatedGifEncoder();
+		    e.start("test_result.gif");
+		    e.setDelay(1000);   // 1 frame per sec
+		    e.addFrame(ImageIO.read(new File("test.png")));
+		    e.addFrame(ImageIO.read(new File("test2.png")));
+		    e.finish();
+		
+		logger.info("animatedGifEncoder : ", e.toString());
+		
+		return "home";
+	}
+	
+	public static BufferedImage toBufferedImage(Image img)
+	{
+	    if (img instanceof BufferedImage)
+	    {
+	        return (BufferedImage) img;
+	    }
+
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+
+	    // Return the buffered image
+	    return bimage;
+	}
+	
+}
